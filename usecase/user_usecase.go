524d80c944533e37f007fa756000dd6b0049fe84
TxxM6 <tomoki.zenphone2@gmail.com>
2024-04-28 14:57:06 +0900

    [v3]validator

--- usecase/user_usecase.go
+++ usecase/user_usecase.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"go-rest-api-handson/model"
 	"go-rest-api-handson/repository"
+	"go-rest-api-handson/validator"
 	"os"
 	"time"
 
@@ -17,15 +18,18 @@ type IUserUsecase interface {
 
 type userUsecase struct {
 	ur repository.IUserRepository
+	uv validator.IUserValidator
 }
 
-func NewUserUsecase(ur repository.IUserRepository) IUserUsecase {
-	return &userUsecase{ur}
+func NewUserUsecase(ur repository.IUserRepository, uv validator.IUserValidator) IUserUsecase {
+	return &userUsecase{ur, uv}
 }
 
 func (uu *userUsecase) SignUp(user model.User) (model.UserResponse, error) {
+	if err := uu.uv.UserValidate(user); err != nil {
+		return model.UserResponse{}, nil
+	}
 	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
-
 	if err != nil {
 		return model.UserResponse{}, err
 	}
@@ -41,7 +45,9 @@ func (uu *userUsecase) SignUp(user model.User) (model.UserResponse, error) {
 }
 
 func (uu *userUsecase) Login(user model.User) (string, error) {
-
+	if err := uu.uv.UserValidate(user); err != nil {
+		return "", err
+	}
 	storedUser := model.User{}
 	if err := uu.ur.GetUserByEmail(&storedUser, user.Email); err != nil {
 		return "", err
